3d04c5565d249c6783def0d45a4ba055531363fa
mhanak96 <mateuszhanak@protonmail.com>
2022-10-30 21:06:58 +0100

    feature mobile version and logic

---
+++ Crowdfunding Product Page Main/main.js
@@ -0,0 +1,57 @@
+['use strict']
+
+const primaryHeader = document.querySelector('.header');
+const navToogle = document.querySelector('.mobile-nav-toogle');
+const primaryNav = document.querySelector('.primary-nav__list');
+
+const btnCta = document.querySelector('.btn-cta')
+const modal = document.querySelector('.modal');
+
+const modalBox =  document.querySelectorAll('.modal-box');
+const options = document.querySelectorAll('.modal__option');
+
+const modalExit = document.querySelector('.modal__close');
+
+const paymentBtns = document.querySelectorAll('.payment__button');
+const succesModal = document.querySelector('.succes-modal');
+const succesBtn = document.querySelector('.succes__button');
+
+navToogle.addEventListener('click', () => {
+    primaryNav.hasAttribute('data-visible') 
+    ? navToogle.setAttribute('aria-expanded', false) 
+    : navToogle.setAttribute('aria-expanded', true);
+    primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-over");
+}); 
+
+
+// modal toogle
+
+btnCta.addEventListener('click', function(){
+    modal.toggleAttribute("open");
+    primaryHeader.toggleAttribute("data-over");
+});
+
+options.forEach(option => option.addEventListener('click', function(){
+    
+    modalBox.forEach(modal => modal.classList.remove('active'));
+
+    let choice = option.getAttribute('choice');
+    document.querySelector(`.${choice}`).classList.add('active');
+    
+}))
+
+modalExit.addEventListener('click', function(){
+    modal.toggleAttribute("open");
+    primaryHeader.toggleAttribute("data-over");
+});
+
+paymentBtns.forEach(pay => pay.addEventListener('click', function(){
+    modal.toggleAttribute("open");
+    succesModal.toggleAttribute('show');
+}))
+
+succesBtn.addEventListener("click", function(){
+    succesModal.toggleAttribute('show');
+    primaryHeader.toggleAttribute("data-over");
+})
